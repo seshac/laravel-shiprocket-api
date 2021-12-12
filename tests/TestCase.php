bc8e20207018453c7e3737502184c07faa589d01
sesha <schandrappa@javelinagency.com>
2021-12-12 21:46:33 +0530

    removed loading legacy factories

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,8 +10,6 @@ class TestCase extends Orchestra
     public function setUp(): void
     {
         parent::setUp();
-
-        $this->withFactories(__DIR__.'/database/factories');
     }
 
     protected function getPackageProviders($app)
